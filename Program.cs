c077f7f5f103189de84a8a0fe3c8cd42b335a8bf
gosaurus <63751262+gosaurus@users.noreply.github.com>
2025-01-15 19:59:40 +0000

    First commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the calculator");
+            Console.WriteLine("=========================");
+
+            Console.WriteLine("Enter your first number: ");
+            string responseOne = Console.ReadLine()!;
+            int numberOne = int.Parse(responseOne);
+
+            Console.WriteLine("Enter your second number: ");
+            string responseTwo = Console.ReadLine()!;
+            int numberTwo = int.Parse(responseTwo);
+
+            int result = numberOne * numberTwo;
+            Console.WriteLine("Your result is: " + result);
+        }
+    }
+}

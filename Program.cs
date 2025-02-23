547bcfc8fc64711a8e5c4917be1be97300fe34fc
gosaurus <63751262+gosaurus@users.noreply.github.com>
2025-02-23 19:33:28 +0000

    Add input validation and while loop

--- Program.cs
+++ Program.cs
@@ -1,24 +1,96 @@
 using System;
 
 namespace Calculator
-{
-    class Program 
+{ class Program 
     {
         static void Main(string[] args)
         {
-            PrintWelcomeMessage();
-            string Operator = GetOperator();
-            
-            int NumberArrayLength = CalculationTimes(Operator);
+            bool ContinueCalculator;
+            do
+            {
+                PrintWelcomeMessage();
+                string Operator = GetOperator();
+                
+                int NumberArrayLength = CalculationTimes(Operator);
 
-            int[] myNumberArray = CreateNumberArray(NumberArrayLength);
+                int[] myNumberArray = CreateNumberArray(NumberArrayLength);
 
-            int result = myNumberArray[0];
+                int result = myNumberArray[0];
 
-            //display myNumberArray
-            Console.Write("[{0}] \n", String.Join(", ", myNumberArray));
+                //display myNumberArray
+                Console.Write("[{0}] \n", String.Join(", ", myNumberArray));
 
-            //Solution using i
+                result = PerformCalculations(result, myNumberArray, Operator);
+
+                Console.WriteLine("Your result is: " + result);
+                
+                ContinueCalculator = Continue();
+            }
+            while (ContinueCalculator);
+        }
+    
+        private static bool Continue()
+        {
+            Console.WriteLine("Do you want to continue? Y/n");
+            string response = Console.ReadLine().Trim();
+            if (response == "n" || response == "N")
+            {
+                return false;
+            }
+            return true;
+        }
+        
+        private static void PrintWelcomeMessage()
+        {
+            Console.WriteLine("Welcome to the calculator");
+            Console.WriteLine("=========================");
+        }
+
+        private static string GetOperator()
+        {
+            Console.WriteLine("Enter your operator:");
+            string Operator = Console.ReadLine()!;
+            if ((Operator == "+") || (Operator == "-") || (Operator == "*") || (Operator == "/"))
+            { 
+                return Operator;
+            } 
+            Console.WriteLine("Invalid input.");
+            return GetOperator();
+        }        
+        
+        private static int CalculationTimes(string Operator)
+        {
+            Console.WriteLine($"How many times do you want to {Operator}?");
+            int parsedIntegerResponse = StringInputNumber();
+            return parsedIntegerResponse;
+        }
+       
+        private static int[] CreateNumberArray(int NumberArrayLength) 
+        {
+            int[] myNumberArray = new int[NumberArrayLength];
+            for (int i = 0; i < myNumberArray.Length; i++)
+            {
+                Console.WriteLine($"Enter number {i + 1}: ");
+                int parsedIntegerResponse = StringInputNumber();
+                myNumberArray[i] = parsedIntegerResponse;
+            }
+
+            return myNumberArray;
+        }
+        
+        private static int StringInputNumber()
+        {
+            if (int.TryParse(Console.ReadLine(), out int parsedIntegerResponse) &&
+                parsedIntegerResponse > 0)
+            {
+                return parsedIntegerResponse;
+            }
+            Console.WriteLine("Invalid input. Please enter a valid number greater than 0.");
+            return StringInputNumber();
+        }
+
+        private static int PerformCalculations (int result, int[] myNumberArray, string Operator)
+        {
             for (int i = 1; i < myNumberArray.Length; i++)
             {
                  if (Operator == "+")
@@ -38,98 +110,7 @@ namespace Calculator
                     result /= myNumberArray[i];
                  }
             }
-
-            //Solution using foreach
-            /*    
-            foreach (int element in myNumberArray)
-            {
-                Console.WriteLine($"Element = {element}");
-                if (Operator == "+") {
-                    result += element;
-                }
-                
-                else if (Operator == "-") {
-                    result -= element;     
-                }
-
-                else if (Operator == "*") {
-                    if (result > 0)
-                    {
-                        result *= element;
-                    }
-                    else
-                    {
-                        result = element;
-                    }
-                }
-
-                else if (Operator == "/") {
-                    if (result > 0)
-                    {
-                        result /= element;
-                    }
-                    else
-                    {
-                        result = element;
-                    }
-                }
-            
-            }
-            */ 
-
-            /* 
-            switch(Operator)
-            {
-                case "+":
-                    result = numberOne + numberTwo;
-                    break;
-                case "-":
-                    result = numberOne - numberTwo;
-                    break;
-                case "*":
-                    result = numberOne * numberTwo;
-                    break;
-                case "/":
-                    result = numberOne / numberTwo;
-                    break;
-            }
-            */            
-
-            Console.WriteLine("Your result is: " + result);
-        }
-        
-        private static void PrintWelcomeMessage()
-        {
-            Console.WriteLine("Welcome to the calculator");
-            Console.WriteLine("=========================");
-        }
-
-        private static string GetOperator()
-        {
-            Console.WriteLine("Enter your operator:");
-            string Operator = Console.ReadLine()!;
-            return Operator;
-        }        
-        
-        private static int CalculationTimes(string Operator)
-        {
-            Console.WriteLine($"How many times do you want to {Operator}?");
-            string input = Console.ReadLine()!;
-            int intInput = int.Parse(input);
-            return intInput;
-        }
-       
-        private static int[] CreateNumberArray(int NumberArrayLength) 
-        {
-            int[] myNumberArray = new int[NumberArrayLength];
-            for (int i = 0; i < myNumberArray.Length; i++)
-            {
-                Console.WriteLine($"Enter your number {i + 1}: ");
-                int intResponse = int.Parse(Console.ReadLine())!;
-                myNumberArray[i] = intResponse;
-            }
-
-            return myNumberArray;
+            return result;
         }
     }
 }
--- obj/Debug/net9.0/CSharpCalculator.AssemblyInfo.cs
+++ obj/Debug/net9.0/CSharpCalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd14d875b254c784cab039f0b11deb859b66ffe4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e29409cacd5a5bfb59df7d8e3d8d7e1ce0bcd1f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
